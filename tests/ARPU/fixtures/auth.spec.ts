7b079e6a1a928963ef0aa395786dc6017e59c749
Strato Ivan Di Luccio <ivan.diluccio@pagopa.it>
2026-06-25 15:05:57 +0200

    [P4PU-1444] implement authentication fixture and add new E2E test cases for authenticated user flows

---
+++ tests/ARPU/fixtures/auth.spec.ts
@@ -0,0 +1,48 @@
+import { test as base, Page } from '@playwright/test';
+import { ARPU_BROKER_URL } from '../../../utils';
+import * as fs from 'fs';
+
+const AUTH_FILE = 'playwright/.auth/user.json';
+const USERNAME = process.env.USER_USERNAME ?? '';
+const PASSWORD = process.env.USER_PASSWORD ?? '';
+
+export const test = base.extend<{ authenticatedPage: Page }>({
+  authenticatedPage: async ({ browser }, use) => {
+
+    const hasValidSession = fs.existsSync(AUTH_FILE);
+
+    if (!hasValidSession) {
+      console.log('No valid session found. Logging in...');
+
+      const context = await browser.newContext();
+      const page = await context.newPage();
+
+      // --- LOGIN FLOW ---
+      await page.goto(`${ARPU_BROKER_URL}/accesso`);
+      await page.getByTestId('logInButton').click();
+
+      await page.getByTestId('spidButton').click();
+      await page.getByTestId('idp-button-https://idp.uat.oneid.pagopa.it').click();
+      await page.getByLabel('Username').click();
+      await page.getByLabel('Username').fill(USERNAME);
+      await page.getByLabel('Password').click();
+      await page.getByLabel('Password').fill(PASSWORD);
+      await page.getByRole('button', { name: 'Accedi' }).click();
+      await page.getByRole('button', { name: 'Do il consenso' }).click();
+
+      // Waiting for redirect to ARPU frontend
+      await page.waitForURL(ARPU_BROKER_URL);
+
+      // Saving state
+      await context.storageState({ path: AUTH_FILE });
+      await context.close();
+    }
+
+    console.log('Session found, creating context');
+    const authContext = await browser.newContext({ storageState: AUTH_FILE });
+    const authPage = await authContext.newPage();
+
+    await use(authPage);
+    await authContext.close();
+  },
+});
--- tests/ARPU/test.spec.ts
+++ tests/ARPU/test.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from '@playwright/test';
-import { SELECTORS } from '../../utils';
+import { test as authTest } from './fixtures/auth.spec'
+import { SELECTORS, ARPU_BROKER_URL } from '../../utils';
 import { NOTICE_API } from '../../utils/api';
 
-const TEST_URL = '/cittadini/ptdemo';
+const TEST_URL = ARPU_BROKER_URL;
 const TEST_IUV_OR_NAV = '50000000001140314';
 const TEST_USER = 'Marco Polo';
 const TEST_CF = 'PLOMRC01P30L736Y';
@@ -14,45 +15,10 @@ const noticeInfo = {
   orgFisacalCode: '99999000013'
 };
 
-test('ARPU-006 - Come cittadino voglio cercare una avviso di pagamento, scaricare il pdf e procedere con il pagamento', async ({
-  page
-}) => {
-  await page.goto(`${TEST_URL}/accesso`);
-  await page.getByTestId(SELECTORS.buttons.login).click();
-
-  // form compilation
-  await page.locator(SELECTORS.noticeSearch.iuvInput).fill(TEST_IUV_OR_NAV);
-  await page.locator(SELECTORS.noticeSearch.cfInput).fill(TEST_CF);
-  await page.locator(SELECTORS.noticeSearch.searchButton).click();
-
-  //download notice pdf
-  await expect(page.locator(SELECTORS.noticeSearch.statusClass)).toBeVisible();
-  const downloadPromise = page.waitForEvent('download');
-  await page.getByTestId(SELECTORS.noticeSearch.downloadButton).click();
-  const download = await downloadPromise;
-
-  expect(download.suggestedFilename()).toContain('.pdf');
-
-  //pay notice
-  await page.goBack();
-  await page.getByTestId(SELECTORS.noticeSearch.payButtonLabel).click();
-
-  //CHECKOUT
-  await page.getByLabel('Apri riepilogo pagamento').click();
-
-  const amount = page.getByText('Importo', { exact: true }).locator('//following-sibling::*[1]');
-  await expect(amount).toHaveText(noticeInfo.amount);
-
-  const ec = page.getByText('Ente Creditore', { exact: true }).locator('//following-sibling::*[1]');
-  await expect(ec).toHaveText(noticeInfo.ec);
-
-  //Simulate successful completion
-  await page.goto(
-    `${TEST_URL}/public/esito/pagamento-avviso-completato?nav=${TEST_IUV_OR_NAV}&org_fiscal_code=${noticeInfo.orgFisacalCode}`
-  );
-  await page.waitForURL(
-    `${TEST_URL}/public/esito/pagamento-avviso-completato?nav=${TEST_IUV_OR_NAV}&org_fiscal_code=${noticeInfo.orgFisacalCode}`
-  );
+authTest('ARPU-002 - Come cittadino voglio scaricare una ricevuta di pagamento', async ({ authenticatedPage }) => {
+  await authenticatedPage.goto(`${TEST_URL}/dashboard`);
+  expect(authenticatedPage.url()).toContain(`${TEST_URL}/dashboard`);
+  // add mores steps according to the ARPU-002 test case
 });
 
 test('ARPU-004 - Come cittadino voglio generare un avviso di pagamento “spontaneo" e procedere con il pagamento', async ({
@@ -138,5 +104,52 @@ test('ARPU-004 - Come cittadino voglio generare un avviso di pagamento “sponta
   );
   await page.waitForURL(
     `${TEST_URL}/public/esito/pagamento-avviso-completato?nav=${nav}&org_fiscal_code=${noticeInfo.orgFisacalCode}`
+  );
+});
+
+authTest('ARPU-005 - Come cittadino voglio recuperare una ricevute di un pagamento che ho effettuato per poter consultare il dettaglio e scaricare il pdf', async ({ authenticatedPage }) => {
+  await authenticatedPage.goto(`${TEST_URL}/dashboard`);
+  expect(authenticatedPage.url()).toContain(`${TEST_URL}/dashboard`);
+  // add mores steps according to the ARPU-005 test case
+});
+
+test('ARPU-006 - Come cittadino voglio cercare una avviso di pagamento, scaricare il pdf e procedere con il pagamento', async ({
+  page
+}) => {
+  await page.goto(`${TEST_URL}/accesso`);
+  await page.getByTestId(SELECTORS.buttons.login).click();
+
+  // form compilation
+  await page.locator(SELECTORS.noticeSearch.iuvInput).fill(TEST_IUV_OR_NAV);
+  await page.locator(SELECTORS.noticeSearch.cfInput).fill(TEST_CF);
+  await page.locator(SELECTORS.noticeSearch.searchButton).click();
+
+  //download notice pdf
+  await expect(page.locator(SELECTORS.noticeSearch.statusClass)).toBeVisible();
+  const downloadPromise = page.waitForEvent('download');
+  await page.getByTestId(SELECTORS.noticeSearch.downloadButton).click();
+  const download = await downloadPromise;
+
+  expect(download.suggestedFilename()).toContain('.pdf');
+
+  //pay notice
+  await page.goBack();
+  await page.getByTestId(SELECTORS.noticeSearch.payButtonLabel).click();
+
+  //CHECKOUT
+  await page.getByLabel('Apri riepilogo pagamento').click();
+
+  const amount = page.getByText('Importo', { exact: true }).locator('//following-sibling::*[1]');
+  await expect(amount).toHaveText(noticeInfo.amount);
+
+  const ec = page.getByText('Ente Creditore', { exact: true }).locator('//following-sibling::*[1]');
+  await expect(ec).toHaveText(noticeInfo.ec);
+
+  //Simulate successful completion
+  await page.goto(
+    `${TEST_URL}/public/esito/pagamento-avviso-completato?nav=${TEST_IUV_OR_NAV}&org_fiscal_code=${noticeInfo.orgFisacalCode}`
+  );
+  await page.waitForURL(
+    `${TEST_URL}/public/esito/pagamento-avviso-completato?nav=${TEST_IUV_OR_NAV}&org_fiscal_code=${noticeInfo.orgFisacalCode}`
   );
 });
