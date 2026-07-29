c597a3e07e2674fb45f259d4804008a3c6a208d6
Strato Ivan Di Luccio <34749257+stratoivandiluccio@users.noreply.github.com>
2026-07-29 09:17:25 +0200

    feat: [P4PU-1465] introducing ARPU-001 e2e test (#17)

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { expect, Page } from '@playwright/test';
+
 // TYPES AND INTERFACES
 export interface Municipality {
   label: string;
@@ -15,11 +17,25 @@ export interface Reason {
 
 export type ReasonResponse = Reason[];
 
+export interface PaymentDetails {
+  amount: string;
+  ec: string;
+  orgFiscalCode: string;
+  nav: string;
+  description?: string;
+  debtPositionId?: string;
+}
+
+// CONSTANTS
 export const userData = {
   name: 'Marco Polo',
-  fiscal_code: 'MRCPLO80A01H501J',
+  fiscal_code: 'PLOMRC01P30L736Y',
   email: 'marcopolo@test.it'
 };
+
+export const ARPU_BROKER_URL = '/cittadini/ptdemo';
+
+export const TEST_PAID_IUV = '350000000001168214';
 
 // SELECTORS
 export const SELECTORS = {
@@ -63,6 +79,30 @@ export const SELECTORS = {
     downloadButton: 'download-payment-notice-button',
     statusClass: '.status__UNPAID',
     payButtonLabel: 'pay-now-button'
+  },
+  sidebar: {
+    home: 'sidebar-menu-item-homepage',
+    debtPositions: 'sidebar-menu-item-debt-positions',
+    receipts: 'sidebar-menu-item-receipts'
+  },
+  debtPositionListPage: {
+    detailButton: 'debt-position-detail-button-:debtPositionId'
+  },
+  receiptListPage: {
+    detailButton: 'receipt-detail-button-:receiptId'
+  },
+  receiptDetailPage: {
+    downloadButton: 'receipt-detail-download-button'
+  },
+  debtPositionDetailPage: {
+    payButton: 'payment-option-action-pay'
+  },
+  searchPage: {
+    goToDetailButton: 'search-item-detail-button'
+  },
+  listItem: {
+    ec: 'list-item-ec',
+    description: 'list-item-description'
   }
 };
 
@@ -83,5 +123,68 @@ export function parseCurrencyToNumber(currencyString: string): number {
   const sanitized = currencyString.replace(/[^\d,.-]/g, '').replace(',', '.');
   return parseFloat(sanitized);
 }
+/**
+ * Simulates the checkout payment process for a given notice.
+ */
+export const simulateCheckoutPayment = async (page: Page, paymentDetails: PaymentDetails) => {
+  await page.getByLabel('Apri riepilogo pagamento').click();
 
-export const ARPU_BROKER_URL = '/cittadini/ptdemo';
+  const amount = page
+    .getByText('Importo', { exact: true })
+    .locator('xpath=following-sibling::*[1]');
+  await expect(amount).toHaveText(paymentDetails.amount);
+
+  const ec = page
+    .getByText('Ente Creditore', { exact: true })
+    .locator('xpath=following-sibling::*[1]');
+  await expect(ec).toHaveText(paymentDetails.ec);
+
+  //Simulate successful completion
+  await page.goto(
+    `${ARPU_BROKER_URL}/public/esito/pagamento-avviso-completato?nav=${paymentDetails.nav}&org_fiscal_code=${paymentDetails.orgFiscalCode}`
+  );
+  await page.waitForURL(
+    `${ARPU_BROKER_URL}/public/esito/pagamento-avviso-completato?nav=${paymentDetails.nav}&org_fiscal_code=${paymentDetails.orgFiscalCode}`
+  );
+};
+
+/**
+ * Finds a debt position by its description using pagination.
+ */
+export const findDebPositionByDescriptionUsingPagination = async (
+  page: Page,
+  description: string
+) => {
+  let found = false;
+  let currentPage = 1;
+  while (!found && currentPage <= 20) {
+    const notices = page.locator('[role="listitem"]');
+    await expect(notices.first()).toBeVisible();
+    const count = await notices.count();
+    console.log(
+      `Checking ${count} debt positions on page ${currentPage} for description: "${description}"`
+    );
+
+    for (let i = 0; i < count; i++) {
+      const noticeDescription = await notices
+        .nth(i)
+        .getByTestId(SELECTORS.listItem.description)
+        .textContent();
+      if (noticeDescription === description) {
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      const nextButton = page.getByLabel('Go to next page');
+      if (!(await nextButton.isVisible())) {
+        break; // No more pages to navigate
+      }
+      await nextButton.click();
+      currentPage++;
+    }
+  }
+
+  return found;
+};
